3e0de5771ebb1f69861331f692e26d99fec7f967
0chilli-dev0 <joaohenriquepp11@gmail.com>
2024-12-29 22:19:21 -0300

    exercicio

---
+++ aula11/ex008.js
@@ -0,0 +1,11 @@
+var vel = Number ( window.prompt ('qual a sua velocidade? "so numero"'))
+if(vel > 80){
+    console.log(`a velocidade do seu carro é ${vel}km/h`)
+    console.log(`voce foi MULTADO`)
+}
+else{
+    console.log(`a velocidade do seu carro é ${vel}km/h`)
+    console.log(`ande com segurança`)
+}
+
+console.log('até 80km/h Deus guarda, mais de 80km/h Deus LHE aguarda')
